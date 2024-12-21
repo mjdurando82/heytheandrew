71b9ac26ff084bf9a917206acb6a6bbb1692a973
Michael Durando <mjdurando82@yahoo.com>
2024-12-21 00:20:22 -0500

    added build

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,13 @@ module.exports = {
         heytheandrew: "url('/public/images/Group.png')",
         callus: "url('/public/images/CALLUS.png')",
         callus2: "url('/public/images/CALLUS2.png')",
+        callus3: "url('/public/images/CALLUS3.png')",
         apArt: "url('/public/images/ApArt.png')",
         apArt2: "url('/public/images/ApArt2.png')",
-        cbs: "url('/public/images/Cbs.png')"
+        cbs: "url('/public/images/Cbs.png')",
+        wader: "url('/public/images/WADER.png')",
+        graphicDesign: "url('/public/images/GraphicDesign.png')",
+        graphicDesign2: "url('/public/images/GraphicDesign2.png')"
       }
     }
   },
